3a674303ac5877a7aedda7066474d86052b9711e
Paulo Victor <paulovictor.devs@gmail.com>
2023-11-16 15:03:46 -0300

    add function to check if theres alredy a task form

--- todo-list/src/pages/Home/PageEvent.js
+++ todo-list/src/pages/Home/PageEvent.js
@@ -1,3 +1,4 @@
+import { checkIfFormExists } from "./CheckExistentForm";
 import addTaskBtnEvent from "./addTasks/events/AddTaskBtn";
 import editTask from "./editTasks/events/EditTaskEvent";
 
@@ -5,6 +6,7 @@ document.addEventListener('click', documentEventListener);
 
 function documentEventListener(e) {
  
+    //checkIfFormExists();
     if(e.target.id === 'add-task-button') addTaskBtnEvent(e);
     if(e.target.closest('.task-left-side-info')) editTask(e);
 
