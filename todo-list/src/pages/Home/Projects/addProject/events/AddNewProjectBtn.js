e3da2c82f13825435fe83f7a988589b54451ff91
Paulo Victor <paulovictor.devs@gmail.com>
2023-12-09 12:46:49 -0300

    fix bug where user had to double click '+ New Project' to open modal

--- todo-list/src/pages/Home/Projects/addProject/events/AddNewProjectBtn.js
+++ todo-list/src/pages/Home/Projects/addProject/events/AddNewProjectBtn.js
@@ -6,8 +6,7 @@ export default function addNewProject(e) {
     //e.preventDefault();
 
     // Opens modal form
-    const addNewProjectBtn = document.getElementById('add-new-project');
-    addNewProjectBtn.addEventListener('click', (e) => openModal());
+    openModal();
 
     // Cancels form and closes modal
     const cancelProjectFormBtn = document.getElementById('project-cancel-btn');
