b99616a7cd4f90de196b9637fd3f5e6721890e3f
Paulo Victor <paulovictor.devs@gmail.com>
2023-12-09 13:23:06 -0300

    /

--- todo-list/src/data/Task.js
+++ todo-list/src/data/Task.js
@@ -9,42 +9,4 @@ export default class Task {
     this.taskId = uuidv4();
     // project must be here too;
   }
-
-  /*
-    get title() {
-        return this._title;
-    }
-    set title(value) {
-        // some validation here? or just on the frontend?
-        this._title = value;
-    }
-
-    get description() {
-        return this._description;
-    }
-    set description(value) {
-        // some validation here? or just on the frontend?
-        this._description = value;
-    }
-
-    get date() {
-        return this._date;
-    }
-    set date(value) {
-        // some validation here? or just on the frontend?
-        this._date = value;
-    }
-
-    get priority() {
-        return this._priority;
-    }
-    set priority(value) {
-        // some validation here? or just on the frontend?
-        this._priority = value;
-    }
-    
-    get id() {
-        return this._id;
-    }
-    */
 }
